4faf40555dcfc44a93076e8611af3bf6fa3c1be9
Tarun1312 <144748933+Tarun1312@users.noreply.github.com>
2024-12-06 16:06:17 -0700

    delete files

--- ReviewHubApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReviewHubApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
